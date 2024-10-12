3525c4a27d6731f1b8688fc597d9340b24ff0374
rohith <8301605+rohith12@users.noreply.github.com>
2024-10-12 09:23:07 -0500

    updating UT

--- WeatherApp/WeatherAppTests/WeatherAppTests.swift
+++ WeatherApp/WeatherAppTests/WeatherAppTests.swift
@@ -35,8 +35,8 @@ final class WeatherAppTests: XCTestCase {
 }
 
 class MockNetworkLayer: NetworkProtocol {
-    func fetchWeather(locationName: String) async throws -> Result<Weather, NetworkError> {
+    func fetchWeather(locationName: String) async throws -> Weather {
         let weatherModel: Weather = Weather(location: Location(name: "Dallas", region: "", country: "USA", lat: 90, lon: 90, tzID: "", localtimeEpoch: 0, localtime: ""), current: Current(lastUpdatedEpoch: 0, lastUpdated: "0", tempC: 35, tempF: 89, isDay: 1, condition: Condition(text: "", icon: "", code: 0), windMph: 0, windKph: 0, windDegree: 0, windDir: "", pressureMB: 0, pressureIn: 0, precipMm: 0, precipIn: 0, humidity: 0, cloud: 0, feelslikeC: 0, feelslikeF: 0, visKM: 0, visMiles: 0, uv: 0, gustMph: 0, gustKph: 0))
-        return .success(weatherModel)
+        return weatherModel
     }
 }
